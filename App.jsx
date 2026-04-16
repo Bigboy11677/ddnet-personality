920d9449594e288815a09c6a8d012c445af25999
Bigboy11677 <141339722+Bigboy11677@users.noreply.github.com>
2026-04-17 02:36:08 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+
+
+// App.jsx 已清理 - 移除了未使用的MBTI测试代码
+export default function App() {
+  return null;
+}
---
+++ main.jsx
@@ -0,0 +1,6 @@
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(React.StrictMode, null, React.createElement(App, null)),
+)
